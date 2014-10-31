459b24be8c719e39267cd39ff660845937956ab3
Anderson Lobo Feitosa <andersonlf@gmail.com>
2014-10-31 17:59:56 -0200

    new challenge resolved and your data file

--- hackerrank/bash-tutorials/compute-the-average/script.sh
+++ hackerrank/bash-tutorials/compute-the-average/script.sh
@@ -2,16 +2,15 @@
 
 read n
 
-soma=0
+sum=0
 for (( c=1; c<=$n; c++ ))
 do
   read r
-  soma=`expr $soma + $r`
+  sum=`expr $sum + $r`
 done
 
-#num=`expr $soma * 1000 / $n`
-num=$(echo "$soma * 1000 / $n" | bc)
+num=$(echo "$sum / $n" | bc -l)
 
-echo `printf %.3f $num`
+echo $num | awk '{printf("%.3f",$1)}'
 
-echo $num
+exit $?
