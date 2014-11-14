27442350cc1f7722bb270cf19681417edd54ddbb
Anderson Lobo Feitosa <andersonlf@gmail.com>
2014-11-14 17:18:49 -0200

    more tests

--- hackerrank/algorithms/warmup/sherlock-and-the-beast/src/main/java/Solution.java
+++ hackerrank/algorithms/warmup/sherlock-and-the-beast/src/main/java/Solution.java
@@ -14,38 +14,49 @@ public class Solution {
 		for (int i = 0; i < t; i++) {
 			int n = in.nextInt();
 			
-			System.out.println(calculateDecentNumber(n, 0));
+			calculateDecentNumber(n);
 		}
 	}
 
-	private static String calculateDecentNumber(int n, int z) {
-		String decentNumber = "";
+	private static void calculateDecentNumber(int n) {
+		StringBuilder sb = new StringBuilder();
 		
-		if (n % 3 == 0 && z % 5 == 0) {
+		if (n < 3) {
+			sb.append("-1");
+//		} else if (n == 3 || n % 3 == 0) {
+//			write5(n, sb);
+//		} else if (n == 5 || n % 5 == 0) {
+//			write3(n, sb);
+		} else {
 			for (int i = 0; i < n; i++) {
-				decentNumber += "5";
+				
+				// qnt 5  
+				int n5 = n - i;
+				int n3 = i;
+				
+				if ((n3 % 5 == 0 && n5 % 3 == 0) || n3 % 5 == 0 || n5 % 3 == 0) {
+					write5(n5, sb);
+					write3(n3, sb);
+				}
+				
 			}
-			for (int i = 0; i < z; i++) {
-				decentNumber += "3";
-			}
-			return decentNumber;
-		} 
-
-		if (n % 5 == 0 && z % 3 == 0) {
-			for (int i = 0; i < n; i++) {
-				decentNumber += "3";
-			}
-			for (int i = 0; i < z; i++) {
-				decentNumber += "5";
-			}
-			return decentNumber;
-		} 
-		
-		if (n == 1) {
-			decentNumber = "-1";
-			return decentNumber;
 		}
 		
-		return calculateDecentNumber(n-1, z + 1);
+		System.out.println(sb.toString());
 	}
+	
+	private static void write3(int n, StringBuilder sb) {
+		write("3", n, sb);
+	}
+
+	private static void write5(int n, StringBuilder sb) {
+		write("5", n, sb);
+	}
+	
+	private static void write(String value, int n, StringBuilder sb) {
+		for (int i = 0; i < n; i++) {
+			sb.append(value);
+		}
+	}
+
 }
