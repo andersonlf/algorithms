07cfb485693aead6196015e7ee74fb62617f4a90
Anderson Lobo Feitosa <andersonlf@gmail.com>
2014-11-21 19:56:14 -0200

    final solution.

--- hackerrank/algorithms/warmup/acm-icpc-team/src/main/java/Solution.java
+++ hackerrank/algorithms/warmup/acm-icpc-team/src/main/java/Solution.java
@@ -28,41 +28,11 @@ public class Solution {
 
 	private static void process(int[][] matrixKnowledge, int n, int m) {
 		discoverNumMaxTopics(matrixKnowledge, n, m);
-		discoverTeamsThatCanKnowTheMaximumNumberOfTopics(matrixKnowledge, n, m);
-	}
-
-	private static void discoverTeamsThatCanKnowTheMaximumNumberOfTopics(int[][] matrixKnowledge, int n, int m) {
-		int countTeamKnowMaxTopics = 0;
-		int[] personN = new int[m];
-		int[] personM = new int[m];
-		
-		for (int i = 0; i < n; i++) {
-			personN = matrixKnowledge[i];
-			
-			for (int j = i; j < n; j++) {
-				personM = matrixKnowledge[j];
-						
-				boolean allKnowledge = true;
-				for(int z = 0; z < m; z++) {
-					int know = personN[z] + personM[z];
-					
-					if (know < 1) {
-						allKnowledge = false;
-						break;
-					}
-				}
-				
-				if (allKnowledge) {
-					countTeamKnowMaxTopics++;
-				}
-			}
-		}
-		
-		System.out.println(countTeamKnowMaxTopics);
 	}
 
 	private static void discoverNumMaxTopics(int[][] matrixKnowledge, int n, int m) {
 		int maxTopics = 0;
+		int numTeams = 0;
 		int[] knowledgePersonA = null;
 		int[] knowledgePersonB = null;
 		for (int i = 0; i < n; i++) {
@@ -80,6 +50,24 @@ public class Solution {
 				}
 			}
 		}
+		
+		for (int i = 0; i < n; i++) {
+			knowledgePersonA = matrixKnowledge[i];
+			for (int j = i+1; j < n; j++) {
+				int topicsTeam = 0;
+				knowledgePersonB = matrixKnowledge[j];
+				for (int t = 0; t < m; t++) {
+					if (knowledgePersonA[t] + knowledgePersonB[t] > 0) {
+						topicsTeam++;
+					}
+				}
+				if(maxTopics == topicsTeam) {
+					numTeams++;
+				}
+			}
+		}
+		
 		System.out.println(maxTopics);
+		System.out.println(numTeams);
 	}
 }
