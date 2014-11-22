e5a631c53657a1655280ac8f36cd035865ca93ed
Anderson Lobo Feitosa <andersonlf@gmail.com>
2014-11-22 08:54:35 -0200

    changes in angry-children

---
+++ hackerrank/algorithms/warmup/angry-children/src/main/java/Solution.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		solve(in);
+		in.close();
+	}
+
+	public static void solve(Scanner in) {
+		long n = in.nextLong();
+		long k = in.nextLong();
+
+		List<Long> packets = new ArrayList<Long>((int)n);
+		for (int i = 0; i < n; i++) {
+			long j = in.nextLong();
+			
+			packets.add(j);
+		}
+		
+		process(packets, n, k);
+	}
+
+	private static void process(List<Long> packets, long n, long k) {
+		List<Long> unfairness = new ArrayList<Long>();
+		Collections.sort(packets);
+		for (int i = 0; i < k; i++) {
+			for (int j = 0; j <)
+			
+			long min = packets.get(i);
+			long max = packets.get(j);
+			
+			unfairness.add(max - min);
+		}
+		Collections.sort(unfairness);
+		System.out.println(unfairness.get(0));
+	}
+}
