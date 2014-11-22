aac6ed1b0615c57201d28fbfe208ac1ed5feb927
Anderson Lobo Feitosa <andersonlf@gmail.com>
2014-11-22 08:18:57 -0200

    problem filling-jars resolved

--- hackerrank/algorithms/warmup/filling-jars/src/main/java/Solution.java
+++ hackerrank/algorithms/warmup/filling-jars/src/main/java/Solution.java
@@ -9,6 +9,19 @@ public class Solution {
 	}
 
 	public static void solve(Scanner in) {
-		// your code must be here
+		long n = in.nextInt();
+		long m = in.nextInt();
+
+		long sum = 0;
+		for (int i = 0; i < m; i++) {
+			long a = in.nextInt();
+			long b = in.nextInt();
+			long k = in.nextInt();
+
+			sum += ((b - a) + 1) * k;
+		}
+		
+		System.out.println((long)sum/n);
 	}
+
 }
