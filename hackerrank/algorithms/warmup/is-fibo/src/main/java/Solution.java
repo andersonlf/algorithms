b3329e3ef411e321c5ee651af2b46a858527069b
Anderson Lobo Feitosa <andersonlf@gmail.com>
2014-11-27 15:58:17 -0200

    is-fibo problem resolved

--- hackerrank/algorithms/warmup/is-fibo/src/main/java/Solution.java
+++ hackerrank/algorithms/warmup/is-fibo/src/main/java/Solution.java
@@ -9,6 +9,26 @@ public class Solution {
 	}
 
 	public static void solve(Scanner in) {
-		// your code must be here
+		long t = in.nextLong();
+		for (int i = 0; i < t; i++) {
+			long n = in.nextLong();
+			isFibo(n);
+		}
+	}
+
+	private static void isFibo(long n) {
+		fibonacci(0l, 1l, n);
+	}
+
+	private static void fibonacci(long a, long b, long n) {
+		long next = a + b;
+		
+		if (next == n) {
+			System.out.println("IsFibo");
+		} else if (next > n) {
+			System.out.println("IsNotFibo");
+		} else {
+			fibonacci(b, next, n);
+		}
 	}
 }
