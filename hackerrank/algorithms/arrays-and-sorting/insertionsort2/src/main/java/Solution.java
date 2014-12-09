a4f071ce0b14fd9451f8e8406073422a0d771510
Anderson Lobo Feitosa <andersonlf@gmail.com>
2014-12-09 19:15:36 -0200

    starting insertionsort2

--- hackerrank/algorithms/arrays-and-sorting/insertionsort2/src/main/java/Solution.java
+++ hackerrank/algorithms/arrays-and-sorting/insertionsort2/src/main/java/Solution.java
@@ -9,6 +9,37 @@ public class Solution {
 	}
 
 	public static void solve(Scanner in) {
-		// your code must be here
+		int s = in.nextInt();
+		int[] ar = new int[s];
+		
+		for (int i = 0; i < s; i++) {
+			ar[i] = in.nextInt();
+		}
+		
+		process(ar);
+	}
+
+	private static void process(int[] ar) {
+		for (int i = 0; i < ar.length; i++) {
+			int a = ar[i];
+			for (int j = i + 1; j < ar.length; j++) {
+				int b = ar[j];
+				if (a > b) {
+					ar[j] = a;
+					ar[i] = b;
+					break;
+				}
+			}
+			print(ar);
+		}
+	}
+
+	private static void print(int[] ar) {
+		StringBuilder sb = new StringBuilder();
+		for (int i : ar) {
+			sb.append(i);
+			sb.append(" ");
+		}
+		System.out.println(sb.toString());
 	}
 }
