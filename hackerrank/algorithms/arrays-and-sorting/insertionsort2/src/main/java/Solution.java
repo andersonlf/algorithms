e92cc7983757ce204244dc4a4928f6f68c89f070
Anderson Lobo Feitosa <andersonlf@gmail.com>
2014-12-09 18:49:48 -0200

    new problems

---
+++ hackerrank/algorithms/arrays-and-sorting/insertionsort2/src/main/java/Solution.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Solution {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		solve(in);
+		in.close();
+	}
+
+	public static void solve(Scanner in) {
+		// your code must be here
+	}
+}
